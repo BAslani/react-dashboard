4d5b04efb62485493e51b8b03242c80b64260bd8
Behzad Aslani <behzadaslani80@gmail.com>
2023-10-25 12:47:10 +0330

    Build: Added Error page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// components
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+
 function App() {
   return (
-    <h1>
-      Hello, dashboeard
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
