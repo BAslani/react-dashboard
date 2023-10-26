a642745f52aa683222c0af7b5d37a205d0dc430a
Behzad Aslani <behzadaslani80@gmail.com>
2023-10-26 11:03:49 +0330

    Feat: Added RecentActivity section.

--- src/components/Data.js
+++ src/components/Data.js
@@ -4,17 +4,19 @@ import styled from 'styled-components'
 import Promotion from './Promotion'
 import TopSelling from './TopSelling'
 import TopUsers from './TopUsers'
+import RecentActivity from './RecentActivity'
 
 const Data = () => {
     return <section>
         <Wrapper className='section-center'>
             <AnalysisChart />
-            <aside className='aside'>
+            <aside className='aside1'>
                 <Promotion />
                 <TopSelling />
             </aside>
-            <aside>
+            <aside className='aside2'>
                 <TopUsers />
+                <RecentActivity />
             </aside>
         </Wrapper>
     </section>
@@ -37,10 +39,16 @@ div {
     width: 100% !important;
 }
 
-.aside {
+.aside1 {
     height: 100%;
     display: grid;
     grid-template-rows: auto 1fr;
+}
+.aside2 {
+    height: 100%;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    column-gap: 1rem;
 }
 `
 
---
+++ src/components/RecentActivity.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { BiCircle } from 'react-icons/bi'
+import styled from 'styled-components'
+
+const RecentActivity = () => {
+    return (
+        <Wrapper>
+            <header>
+                <h5>random activity</h5>
+                <select name="sort" id="sort-activity">
+                    <option value="Yearly">All Members</option>
+                </select>
+            </header>
+            <darticle className="activity-info">
+                <BiCircle />
+                <div className="text">
+                    <p>Today 12:20 pm</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ex.</p>
+                </div>
+            </darticle>
+            <article className="activity-info">
+                <BiCircle />
+                <div className="text">
+                    <p>Today 12:20 pm</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ex.</p>
+                </div>
+            </article>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.article`
+border-radius: var(--radius);
+padding: 1rem 2rem;
+background: var(--clr-white);
+box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.1);
+
+header {
+    h5 {
+      margin: 0;
+    }
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 0.75rem;
+  }
+  #sort-activity {
+    background: transparent;
+    border: none;
+  }
+
+  .activity-info {
+    p {
+        margin: 0;
+    }
+
+    display: grid;
+    grid-template-columns: auto 1fr;
+    column-gap: 1rem;
+    margin-bottom: 1rem;
+  }
+`
+
+export default RecentActivity
